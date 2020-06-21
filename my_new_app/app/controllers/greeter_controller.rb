31c39863270654834f1e399574b828a44a4cece5
Maxsash <the.revived.1@gmail.com>
2020-06-21 16:14:54 +0530

    Moving business logic out of view

--- my_new_app/app/controllers/greeter_controller.rb
+++ my_new_app/app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
   def hello
+		names = ["Mike", "Scotty", "Rodman"]
+		@name = names.sample
+		@time = Time.now
+		@times_displayed ||= 0
+		@times_displayed += 1
   end
 	def goodbye
 	end
